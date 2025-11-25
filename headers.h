7404840b77b57f988ab7ac2d9695804a2b86efcf
Ziadkh06 <ziadkhaledsaif@gmail.com>
2025-11-25 15:03:16 +0200

    HPF
    doing HPF and its helper functions

--- headers.h
+++ headers.h
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
+#include <errno.h>
 
 typedef short bool;
 #define true 1
@@ -41,6 +43,161 @@ int getClk()
     return *shmaddr;
 }
 
+//---------------------------------------ammar's additions---------------------------------------//
+
+
+/* Globals required by scheduler.c */
+int msgq_id;                              /* message queue id (shared ma3 generator) */
+PCB pcbs[MAX_PROCESSES];                  /* bt store active PCBs indexed bel UID-1 */
+bool ran[MAX_PROCESSES];                  /* process run wla la */
+FILE *log_file = NULL;                    /* scheduler log file */
+volatile sig_atomic_t usr1_received = 0;  /* set bel SIGUSR1 handler lma el chile ykhallas*/
+int current_pcb_index = -1;               /* index of currently running PCB (UID-1) */
+
+
+PCB TO_PCB(Process_Param p) {
+    PCB pcb;
+    memset(&pcb, 0, sizeof(PCB));
+    pcb.state = READY;
+    pcb.PID = -1;
+    pcb.UID = p.ID;
+    pcb.PRIORITY = p.Priority;
+    pcb.ARRIVAL_TIME = p.Arrival_Time;
+    pcb.RUN_TIME = p.runtime;
+    pcb.REMAINING_TIME = p.runtime;
+    pcb.START_TIME = pcb.END_TIME = pcb.WAIT_TIME = pcb.TURNAROUND_TIME = 0;
+    pcb.RESUME_TIME = 0;
+
+    return pcb;
+}
+
+
+int CREATE_PROCESS(int runtime, int uid) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return -1;
+    }
+    if (pid == 0) {
+        
+        char runtime_s[32], uid_s[32];
+        snprintf(runtime_s, sizeof(runtime_s), "%d", runtime);
+        snprintf(uid_s, sizeof(uid_s), "%d", uid);
+
+        execl("./process.out", "process.out", runtime_s, uid_s, NULL);
+        
+        perror("execl(process.out)");
+        _exit(1);
+    }
+    
+    return (int)pid;
+}
+
+
+void sigusr1_handler(int signo) {
+    (void)signo;
+    usr1_received = 1;
+    if (current_pcb_index >= 0 && current_pcb_index < MAX_PROCESSES) {
+        int now = getClk();
+        pcbs[current_pcb_index].END_TIME = now;
+        pcbs[current_pcb_index].TURNAROUND_TIME =
+            pcbs[current_pcb_index].END_TIME - pcbs[current_pcb_index].ARRIVAL_TIME;
+    }
+}
+
+void sigchld_handler(int signo) {
+    (void)signo;
+    while (waitpid(-1, NULL, WNOHANG) > 0) { }
+}
+
+void init_scheduler_globals(void) {
+    for (int i = 0; i < MAX_PROCESSES; ++i) {
+        ran[i] = false;
+        pcbs[i].PID = -1;
+    }
+
+    log_file = fopen(LOG_FILE, "w");
+    if (!log_file) {
+        perror("fopen(" LOG_FILE ")");
+        
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = sigusr1_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    struct sigaction sc;
+    sc.sa_handler = sigchld_handler;
+    sigemptyset(&sc.sa_mask);
+    sc.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sigaction(SIGCHLD, &sc, NULL);
+}
+
+
+typedef struct PCB_HEAP {
+    PCB* arr;
+    int size;
+    int capacity;
+    bool remaining_time;
+}PCB_HEAP;
+
+
+PCB_HEAP* PCB_HEAP_INIT(bool runtime) {
+    PCB_HEAP* heap = (PCB_HEAP*) malloc(sizeof(PCB_HEAP));
+    heap->arr = (PCB*) malloc(sizeof(PCB) * DEFAULT_HEAP_SIZE);
+    heap->capacity = DEFAULT_HEAP_SIZE;
+    heap->size = 0;
+    heap->remaining_time = runtime;
+    return heap;
+}
+
+void PCB_HEAP_INSERT(PCB_HEAP* heap, PCB obj) {
+    if (heap->size == heap->capacity) {
+        heap->capacity *= 2;
+        heap->arr = (PCB*) realloc(heap->arr, sizeof(PCB) * heap->capacity);
+    }
+    heap->arr[heap->size++] = obj;
+    push_up(heap, heap->size-1);
+
+}
+
+PCB PCB_HEAP_EXTRACT(PCB_HEAP* heap) {
+    if (heap->size == 0) {
+        PCB temp = {0};
+        return temp;
+    }
+
+    PCB temp = heap->arr[0];
+    heap->arr[0] = heap->arr[--heap->size];
+    push_down(heap, 0);
+    return temp;
+}
+
+PCB PCB_HEAP_PEEK_MIN(PCB_HEAP* heap) {
+    if (heap->size != 0) {
+        PCB temp = heap->arr[0];
+        return temp;
+    }
+    PCB temp;
+    return temp;
+}
+
+bool PCB_HEAP_IS_EMPTY(PCB_HEAP* heap) {
+    return heap->size == 0;
+}
+
+void PCB_HEAP_DESTROY(PCB_HEAP* heap) {
+    free(heap->arr);
+    free(heap);
+}
+
+int PCB_HEAP_SIZE(PCB_HEAP* heap) {
+    return heap->size;
+}
+
+//------------------------------ammar's additions end------------------------------//
 
 /*
  * All process call this function at the beginning to establish communication between them and the clock module.
@@ -76,3 +233,5 @@ void destroyClk(bool terminateAll)
         killpg(getpgrp(), SIGINT);
     }
 }
+
+
--- scheduler.c
+++ scheduler.c
@@ -1,5 +1,133 @@
 #include "headers.h"
 
+//Scheduling Algorithms----------------------------------------------------
+void HIGHEST_PRIORITY_FIRST() {
+
+    Message msg;
+    bool generator_done = false;
+    ready_q = PCB_HEAP_INIT(false); 
+    PCB current_process;
+    int now;
+
+    while (!generator_done || !PCB_HEAP_IS_EMPTY(ready_q)) {
+        
+        while (msgrcv(msgq_id, &msg, sizeof(Message), MSG_NEW_PROCESS, IPC_NOWAIT) != -1) {
+            if (msg.param.ID != -1) {
+                PCB_HEAP_INSERT(ready_q, TO_PCB(msg.param));
+                printf("Recieved process %d\n", msg.param.ID);
+            } else {
+                generator_done = true;
+            }
+        }
+
+        if (!PCB_HEAP_IS_EMPTY(ready_q)) {
+            
+            current_process = PCB_HEAP_EXTRACT(ready_q);
+            now = getClk();
+
+            
+            if (!ran[current_process.UID - 1]) {
+                current_process.START_TIME = now;
+                current_process.RESUME_TIME = now;
+                current_process.WAIT_TIME = current_process.START_TIME - current_process.ARRIVAL_TIME;
+                current_process.state = RUNNING;
+                fprintf(log_file, "At time %d process %d started arr %d total %d remain %d wait %d\n",
+                        now,
+                        current_process.UID,
+                        current_process.ARRIVAL_TIME,
+                        current_process.RUN_TIME,
+                        current_process.RUN_TIME,
+                        current_process.WAIT_TIME);
+
+                current_pcb_index = current_process.UID - 1;
+                fflush(log_file);
+
+                current_process.PID = CREATE_PROCESS(current_process.RUN_TIME, current_process.UID);
+                pcbs[current_pcb_index] = current_process;
+                ran[current_pcb_index] = true;
+            } else {
+                
+                printf("Resuming process %d\n", current_process.UID);
+                current_process = pcbs[current_process.UID - 1];
+                current_process.RESUME_TIME = now;
+                current_process.state = RUNNING;
+                fprintf(log_file, "At time %d process %d resumed arr %d total %d remain %d wait %d\n",
+                        now,
+                        current_process.UID,
+                        current_process.ARRIVAL_TIME,
+                        current_process.RUN_TIME,
+                        current_process.REMAINING_TIME,
+                        current_process.WAIT_TIME);
+                current_pcb_index = current_process.UID - 1;
+                pcbs[current_pcb_index] = current_process;
+                
+                if (kill(current_process.PID, SIGCONT) == -1) {
+                    perror("Error resuming process");
+                }
+            }
+
+            
+            while (1) {
+                if (msgrcv(msgq_id, &msg, sizeof(Message), MSG_NEW_PROCESS, IPC_NOWAIT) != -1) {
+                    now = getClk();
+                    if (msg.param.ID != -1) {
+                        PCB new_process = TO_PCB(msg.param);
+
+                        int elapsed = now - current_process.RESUME_TIME;
+                        if (elapsed < 0) elapsed = 0;
+                        current_process.REMAINING_TIME -= elapsed;
+                        if (current_process.REMAINING_TIME < 0) current_process.REMAINING_TIME = 0;
+                        pcbs[current_process.UID - 1] = current_process;
+
+                        if (new_process.PRIORITY < current_process.PRIORITY) {
+                            if (kill(current_process.PID, SIGSTOP) == -1) {
+                                printf("Process %d could not be stopped with signal %d\n", current_process.UID, SIGSTOP);
+                            } else {
+                                printf("Stopping process %d (preempted by %d)\n", current_process.UID, new_process.UID);
+                                fprintf(log_file, "At time %d process %d stopped arr %d total %d remain %d wait %d\n",
+                                        now,
+                                        current_process.UID,
+                                        current_process.ARRIVAL_TIME,
+                                        current_process.RUN_TIME,
+                                        current_process.REMAINING_TIME,
+                                        current_process.WAIT_TIME);
+                                fflush(log_file);
+                                PCB_HEAP_INSERT(ready_q, current_process);
+                                PCB_HEAP_INSERT(ready_q, new_process);
+                                break; 
+                            }
+                        } else {
+                            PCB_HEAP_INSERT(ready_q, new_process);
+                        }
+                    } else {
+                        generator_done = true;
+                    }
+                }
+
+                usleep(1000 * 500);
+
+                
+                if (usr1_received) {
+                    usr1_received = 0;
+                    break;
+                }
+            }
+        } else {
+            if (!generator_done) {
+                if (msgrcv(msgq_id, &msg, sizeof(Message), MSG_NEW_PROCESS, !IPC_NOWAIT) != -1) {
+                    if (msg.param.ID != -1) {
+                        PCB_HEAP_INSERT(ready_q, TO_PCB(msg.param));
+                    } else {
+                        generator_done = true;
+                    }
+                }
+            }
+        }
+    }
+
+    PCB_HEAP_DESTROY(ready_q);
+}
+
 
 int main(int argc, char * argv[])
 {
